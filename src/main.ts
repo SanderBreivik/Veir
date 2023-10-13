269fa31083872cf4b40014d25eda386facad3ec5
Sander Breivik <sander@acos.no>
2023-10-13 13:33:31 +0200

    Try to fix shoelace #3

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { setBasePath } from "@shoelace-style/shoelace";
 import "./dashboard.ts";
 import "./geolocation.ts";
+import "@shoelace-style/shoelace/dist/shoelace.js";
+import "@shoelace-style/shoelace/dist/themes/dark.css";
 
 // This path should lead to your node_modules folder or where the Shoelace assets are located.
 setBasePath("/node_modules/@shoelace-style/shoelace/dist/");
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { copy } from "vite-plugin-copy";
-
 export default {
-  plugins: [
-    copy({
-      targets: [
-        {
-          src: "node_modules/@shoelace-style/shoelace/dist/*",
-          dest: "public/shoelace",
-        },
-      ],
-      hook: "writeBundle", // ensures the assets are copied during the build
-    }),
-  ],
+  build: {
+    rollupOptions: {
+      external: ["/shoelace/shoelace.js"],
+    },
+  },
 };
