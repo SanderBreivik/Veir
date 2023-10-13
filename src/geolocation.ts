f91741e889c105512dd98c8b36e73fb2fdedab11
Sander Breivik <sander@acos.no>
2023-10-13 08:54:10 +0200

    Add icon and fix shoelaze loading assets

--- src/geolocation.ts
+++ src/geolocation.ts
@@ -41,6 +41,80 @@ export class VeirGeoLocation extends LitElement {
     );
   }
 
+  weatherToSlIcon(weather: string) {
+    var icon = "";
+    switch (weather) {
+      case "clearsky_day":
+      case "clearsky_night":
+      case "clearsky_polartwilight":
+        return "sun";
+
+      case "fair_day":
+      case "fair_night":
+      case "fair_polartwilight":
+      case "partlycloudy_day":
+      case "partlycloudy_night":
+      case "partlycloudy_polartwilight":
+        return "cloud-sun";
+
+      case "cloudy":
+        return "cloud";
+
+      case "rainshowers_day":
+      case "rainshowers_night":
+      case "rainshowers_polartwilight":
+      case "lightrain":
+      case "lightrainshowers_day":
+      case "lightrainshowers_night":
+      case "lightrainshowers_polartwilight":
+      case "rain":
+        return "cloud-drizzle";
+
+      case "heavyrain":
+      case "heavyrainshowers_day":
+      case "heavyrainshowers_night":
+      case "heavyrainshowers_polartwilight":
+        return "cloud-rain-heavy";
+
+      case "rainshowersandthunder_day":
+      case "rainshowersandthunder_night":
+      case "rainshowersandthunder_polartwilight":
+      case "rainandthunder":
+        return "cloud-lightning-rain";
+
+      case "snow":
+      case "snowshowers_day":
+      case "snowshowers_night":
+      case "snowshowers_polartwilight":
+      case "lightsnow":
+      case "lightsnowshowers_day":
+      case "lightsnowshowers_night":
+      case "lightsnowshowers_polartwilight":
+        return "cloud-snow";
+
+      case "fog":
+        return "fog";
+
+      case "sleet":
+      case "lightsleet":
+      case "heavysleet":
+      case "sleetshowers_day":
+      case "sleetshowers_night":
+      case "sleetshowers_polartwilight":
+        return "cloud-hail";
+
+      case "snowandthunder":
+      case "snowshowersandthunder_day":
+      case "snowshowersandthunder_night":
+      case "snowshowersandthunder_polartwilight":
+        return "cloud-lightning-snow";
+
+      default:
+        console.log("No match for weather: " + weather);
+        return "question"; // 'question' is a generic icon if there's no match.
+    }
+  }
+
   handleError(error: GeolocationPositionError) {
     switch (error.code) {
       case error.PERMISSION_DENIED:
@@ -69,15 +143,20 @@ export class VeirGeoLocation extends LitElement {
         ? html`<div>
             Din lokasjon: Breddegrad: ${this.latitude}, Lengdegrad:
             ${this.longitude}
-            <br />
-            Vær:
+            <sl-icon
+              style="font-size: 5rem;"
+              name="${this.weatherToSlIcon(
+                this.weatherData.properties.timeseries[0].data.next_12_hours
+                  .summary.symbol_code
+              )}"
+            ></sl-icon>
             <sl-tree>
               ${this.weatherData.properties.timeseries.map((time) => {
                 return html`<sl-tree-item>
                   ${moment(time.time).fromNow()}:
                   <sl-tree-item
-                    >${time.data.instant.details.air_temperature}</sl-tree-item
-                  >
+                    >${time.data.instant.details.air_temperature}
+                  </sl-tree-item>
                 </sl-tree-item>`;
               })}
             </sl-tree>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,4 @@ import "./dashboard.ts";
 import "./geolocation.ts";
 
 // This path should lead to your node_modules folder or where the Shoelace assets are located.
-setBasePath("/node_modules/@shoelace-style/shoelace/dist/shoelace");
+setBasePath("/node_modules/@shoelace-style/shoelace/dist/");
