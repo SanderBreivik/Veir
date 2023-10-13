114024724d8889ad17b758166a624e156e00db14
Sander Breivik <sander@acos.no>
2023-10-13 14:54:11 +0200

    Fix shoelace

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,4 @@ import "@shoelace-style/shoelace/dist/components/card/card.js";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 
 // Set the base path to the folder you copied Shoelace's assets to
-setBasePath("shoelace/dist");
+setBasePath("dist/shoelace/");
