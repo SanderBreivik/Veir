ce54676faf8268f2f542e883a721b588f8c5037c
Sander Breivik <sander@acos.no>
2023-10-16 07:52:00 +0200

    Remove WYSIWYG debug and add console log to yr api

--- src/services/yr.service.ts
+++ src/services/yr.service.ts
@@ -2,6 +2,7 @@ import { Feature } from "../models/yr.model";
 
 export default class YrService {
   static async getYrWeatherData(lat: string, long: string): Promise<Feature> {
+    console.log("Fetching weather data", lat, long);
     const response = await fetch(
       `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`,
       {
@@ -10,7 +11,9 @@ export default class YrService {
         },
       }
     );
+    console.log("Response: ", response.status, response.statusText);
     const data = await response.json();
+    console.log("Data: ", data);
     return data;
   }
 }
