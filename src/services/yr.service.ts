56f44418617676b3959067563793dd8c8c852f77
Sander Breivik <sander@acos.no>
2023-10-16 08:29:43 +0200

    Fix refferer

--- src/services/yr.service.ts
+++ src/services/yr.service.ts
@@ -3,37 +3,22 @@ import { Feature } from "../models/yr.model";
 export default class YrService {
   static async getYrWeatherData(lat: string, long: string): Promise<Feature> {
     console.log("Fetching weather data", lat, long);
-
-    return new Promise<Feature>((resolve, reject) => {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open(
-          "GET",
-          `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`
-        );
-        xhr.setRequestHeader("User-Agent", "veir");
-
-        xhr.onload = function () {
-          if (xhr.status === 200) {
-            console.log("Data:", JSON.parse(xhr.responseText));
-            resolve(JSON.parse(xhr.responseText) as Feature);
-          } else {
-            console.error("Request failed. Returned status:", xhr.status);
-            reject(new Error("Request failed. Returned status: " + xhr.status));
-          }
-        };
-
-        xhr.onerror = function () {
-          console.error("Network error.");
-          reject(new Error("Network error."));
-        };
-
-        xhr.send();
-      } catch (error: any) {
-        console.error(error);
-        console.error(error.message);
-        reject(new Error("Could not fetch weather data"));
-      }
-    });
+    try {
+      const response = await fetch(
+        `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${long}`,
+        {
+          headers: {
+            "User-Agent": "veir.sanderbreivik.no me@sanderbreivik.no",
+          },
+        }
+      );
+      console.log("Response: ", response.status, response.statusText);
+      const data = await response.json();
+      console.log("Data: ", data);
+      return data;
+    } catch (error: any) {
+      console.error(error.message);
+      throw new Error("Could not fetch weather data");
+    }
   }
 }
